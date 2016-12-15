70dbed8fef25a904a77a895b199f7b1f82d387c1
Caleb Hoover <choover11@gmail.com>
2016-12-14 18:03:42 -0800

    added continent points and tweaked points calculation. Maybe done?

--- scripts/updateTables.sql
+++ scripts/updateTables.sql
@@ -1,23 +1,31 @@
 START TRANSACTION;
 
+CREATE INDEX id ON Countries(id);
+CREATE INDEX id ON Continents(id);
 CREATE INDEX compId on Results(competitionId);
+
+## Attaches Dates and continent ids to results
 
 DROP TABLE IF EXISTS ResultDates;
 CREATE TABLE ResultDates AS (
-	SELECT r.personId, r.personName, r.personCountryId, r.competitionId, r.eventId, r.roundId, r.formatId, r.pos, r.average, r.best, r.regionalAverageRecord, r.regionalSingleRecord,
+	SELECT R.personId, R.personName, R.personCountryId, c.continentId personContinentId, R.competitionId, R.eventId, R.roundId, R.formatId, R.pos, R.average, R.best, R.regionalAverageRecord, R.regionalSingleRecord,
 	DATE_ADD(MAKEDATE(year, day), INTERVAL month-1 MONTH) date,
 	DATE_SUB(DATE_ADD(MAKEDATE(year, day), INTERVAL month-1 MONTH), INTERVAL DAYOFWEEK(DATE_ADD(MAKEDATE(year, day), INTERVAL month-1 MONTH))-1 DAY) weekend
-	FROM Results r
-	JOIN Competitions c ON c.id = r.competitionId
+	FROM Results R
+	JOIN Competitions comps ON comps.id = R.competitionId
+	JOIN (SELECT Countries.id, Continents.recordName continentId FROM Countries LEFT JOIN Continents ON Countries.continentid = Continents.id) c ON c.id = R.personCountryId
 );
+
 
 # ~30
 
 CREATE INDEX eventRound ON ResultDates(eventId, roundId);
 CREATE INDEX roundEventComp ON ResultDates(roundId, eventId, competitionId);
 CREATE INDEX eventCountryDate ON ResultDates(eventId, personCountryId, date);
-CREATE INDEX eventCountryWeekend ON ResultDates(eventId, personCountryId, weekend);
+CREATE INDEX eventContinentDate ON ResultDates(eventId, personContinentId, date);
 CREATE INDEX eventDateAvg ON ResultDates(eventId, date, average);
+CREATE INDEX avg ON ResultDates(average);
+CREATE INDEX best ON ResultDates(best);
 CREATE INDEX rar ON ResultDates(regionalAverageRecord);
 CREATE INDEX rsr ON ResultDates(regionalSingleRecord);
 
@@ -29,23 +37,28 @@ SELECT R.competitionId, R.personId, R.personName, R.personCountryId, R.eventId, 
 @compPoints := TRUNCATE(if(formatId in ('a', 'm'),
 if(R.average > 0, 100 * (comp.bestAvg / (R.average + comp.avg) + comp.bestAvg / R.average), 0),
 if(R.best > 0, 100 * (comp.bestSingle / (R.best + comp.avgSingle) + comp.bestSingle / R.best), 0)), 2) AS compPoints,
-@worldPoints := TRUNCATE(if(R.formatId in ('a', 'm') AND R.average > 0 AND R.best > 0, 50 * (world.average / R.average + world.best / R.best), if(R.best > 0, 50 * world.best / R.best, 0)), 2) AS worldPoints,
-@countryPoints := TRUNCATE(if(R.formatId in ('a', 'm') AND R.average > 0 AND R.best > 0, 50 * (country.average / R.average + country.best / R.best), if(R.best > 0, 50 * country.best / R.best, 0)), 2) AS countryPoints,
-TRUNCATE((@compPoints + @worldPoints + @countryPoints) / 3, 2) AS totalPoints
+@WPoints := TRUNCATE(if(R.formatId in ('a', 'm') AND R.average > 0 AND R.best > 0, 50 * (W.average / R.average + W.best / R.best), if(R.best > 0, 50 * W.best / R.best, 0)), 2) AS worldPoints,
+@CPoints := TRUNCATE(if(R.formatId in ('a', 'm') AND R.average > 0 AND R.best > 0, 50 * (C.average / R.average + C.best / R.best), if(R.best > 0, 50 * C.best / R.best, 0)), 2) AS continentPoints,
+@NPoints := TRUNCATE(if(R.formatId in ('a', 'm') AND R.average > 0 AND R.best > 0, 50 * (N.average / R.average + N.best / R.best), if(R.best > 0, 50 * N.best / R.best, 0)), 2) AS nationPoints,
+TRUNCATE((@compPoints + (@WPoints + 2 * @CPoints + 3 * @NPoints) / 6) / 2, 2) AS totalPoints
 FROM ResultDates R
 LEFT JOIN (SELECT competitionId, eventId, roundId, AVG(r.average) avg, AVG(r.best) avgSingle, MIN(r.average) bestAvg, MIN(r.best) bestSingle
 	FROM ResultDates r WHERE if(formatId in ('a', 'm'), r.average > 0, r.best > 0) GROUP BY competitionId, eventId, roundId) comp ON R.competitionId=comp.competitionId AND R.eventId=comp.eventId AND R.roundId=comp.roundId
 LEFT JOIN (SELECT date, eventId,
 	(SELECT MIN(average) FROM ResultDates r where r.date <= rd.date AND r.eventId=rd.eventId AND r.average > 0 AND regionalAverageRecord='WR') average,
 	(SELECT MIN(best) FROM ResultDates r where r.date <= rd.date AND r.eventId=rd.eventId AND r.best > 0 AND regionalSingleRecord='WR') best
-	FROM (SELECT date,eventId FROM ResultDates GROUP BY date,eventId) rd) world ON world.eventId = R.eventId AND world.date = R.date
+	FROM (SELECT date,eventId FROM ResultDates GROUP BY date,eventId) rd) W ON W.eventId = R.eventId AND W.date = R.date
 LEFT JOIN (SELECT date, eventId, personCountryId,
-	(SELECT MIN(average) FROM ResultDates r where r.date <= rd.date AND r.eventId=rd.eventId AND r.personCountryId = rd.personCountryId AND r.average > 0) average,
-	(SELECT MIN(best) FROM ResultDates r where r.date <= rd.date AND r.eventId=rd.eventId AND r.personCountryId = rd.personCountryId AND r.best > 0) best
-	FROM (SELECT date,eventId,personCountryId FROM ResultDates GROUP BY date,eventId,personCountryId) rd) country ON country.eventId = R.eventId AND country.personCountryId = R.personCountryId AND country.date = R.date
+	(SELECT MIN(average) FROM ResultDates r where r.date <= rd.date AND r.personCountryId = rd.personCountryId AND NOT r.regionalAverageRecord = '' AND r.eventId=rd.eventId AND r.average > 0) average,
+	(SELECT MIN(best) FROM ResultDates r where r.date <= rd.date AND r.personCountryId = rd.personCountryId AND NOT r.regionalSingleRecord = '' AND r.eventId=rd.eventId AND r.best > 0) best
+	FROM (SELECT date,eventId,personCountryId FROM ResultDates GROUP BY date,eventId,personCountryId) rd) N ON N.eventId = R.eventId AND N.personCountryId = R.personCountryId AND N.date = R.date
+LEFT JOIN (SELECT date, eventId, personContinentId,
+	(SELECT MIN(average) FROM ResultDates r where r.date <= rd.date AND r.personContinentId = rd.personContinentId AND NOT r.regionalAverageRecord = '' AND r.eventId=rd.eventId AND r.average > 0) average,
+	(SELECT MIN(best) FROM ResultDates r where r.date <= rd.date AND r.personContinentId = rd.personContinentId AND NOT r.regionalSingleRecord = '' AND r.eventId=rd.eventId AND r.best > 0) best
+	FROM (SELECT date,eventId,personContinentId FROM ResultDates GROUP BY date,eventId,personContinentId) rd) C ON C.eventId = R.eventId AND C.personContinentId = R.personContinentId AND C.date = R.date
 );
 
-# ~10 minutes
+# ~37 minutes, not too bad. Need improving though.
 
 CREATE INDEX week ON Points(week);
 CREATE INDEX year ON Points(year);
