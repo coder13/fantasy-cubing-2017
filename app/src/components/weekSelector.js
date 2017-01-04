bba1e5f7d8345e1efc5755c9a0cea857287d02d0
Caleb Hoover <choover11@gmail.com>
2017-01-04 14:55:12 -0800

    fixed week selector saying week is 7 days rather than 6

--- app/src/components/weekSelector.js
+++ app/src/components/weekSelector.js
@@ -5,7 +5,7 @@ const format = date => date.format('MMMM Do');
 
 module.exports = function (props) {
 	let start = moment().week(props.week).day(4);
-	let end = moment().week(props.week).day(4).add(7, 'days');
+	let end = moment().week(props.week).day(4).add(6, 'days');
 
 	return (
 		<Menu pagination>
