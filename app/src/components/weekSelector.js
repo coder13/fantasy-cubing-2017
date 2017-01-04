a84ae51c590a538e8d4fb7d21f40537e35ab5716
Caleb Hoover <choover11@gmail.com>
2017-01-04 13:05:38 -0800

    shows date of week on week selector

--- app/src/components/weekSelector.js
+++ app/src/components/weekSelector.js
@@ -1,11 +1,18 @@
-const {Menu} = require('semantic-ui-react');
+const moment = require('moment');
+const {Menu, Item} = require('semantic-ui-react');
+
+const format = date => date.format('MMMM Do');
 
 module.exports = function (props) {
+	let start = moment().week(props.week).day(4);
+	let end = moment().week(props.week).day(4).add(7, 'days');
+
 	return (
 		<Menu pagination>
 			<Menu.Item icon='left chevron' onClick={props.last}/>
 			<Menu.Item disabled name={`Week: ${props.week}`}/>
 			<Menu.Item icon='right chevron' onClick={props.next}/>
+			<Item as={Menu.Item} disabled>{format(start)} - {format(end)}</Item>
 		</Menu>
 	);
 };
